011255be25349a7f7e1e734ad259e54061f9facc
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-07-27 22:27:49 +0300

    feature: add hr

--- components/Work/index.js
+++ components/Work/index.js
@@ -3,27 +3,30 @@ import Image from "next/image";
 const Work = (props) => {
   const { img, title, date, tag, description } = props;
   return (
-    <div className="flex flex-col gap-4 pb-8 md:flex-row">
-      <div className="relative h-44 w-64">
-        <Image
-          src={img}
-          alt="screen"
-          layout="fill"
-          objectFit="contain"
-          className="rounded-sm"
-        />
-      </div>
-      <div className="flex flex-col gap-4">
-        <h1 className="text-xl font-bold">{title}</h1>
-        <div className="flex flex-row gap-4 items-center">
-          <div className="chip">
-            <p className="text-bold uppercase">{date}</p>
-          </div>
-          <p className="text-gray-400">{tag}</p>
+    <>
+      <div className="flex flex-col gap-4 md:flex-row">
+        <div className="relative h-44 w-64">
+          <Image
+            src={img}
+            alt="screen"
+            layout="fill"
+            objectFit="contain"
+            className="rounded-sm"
+          />
         </div>
-        <p className="max-w-sm md:max-w-md lg:max-w-lg">{description}</p>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-xl font-bold">{title}</h1>
+          <div className="flex flex-row gap-4 items-center">
+            <div className="chip">
+              <p className="text-bold uppercase">{date}</p>
+            </div>
+            <p className="text-gray-400">{tag}</p>
+          </div>
+          <p className="max-w-sm md:max-w-md lg:max-w-lg">{description}</p>
+        </div>
       </div>
-    </div>
+      <hr />
+    </>
   );
 };
 
