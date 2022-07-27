e147d0b958230be80c5df18e19dcc6021d48d9f8
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-07-28 00:03:27 +0300

    feature: add works

--- components/Works/index.js
+++ components/Works/index.js
@@ -6,10 +6,27 @@ const Works = () => {
       <p className="text-2xl pb-10">Featured works</p>
       <Work
         img="/alias.png"
+        url="https://aliasgame.site"
         title="Alias"
         date="2021"
-        tag="React, Tailwind"
+        tag="React, TS, Tailwind"
         description="Web-version Alias game"
+      />
+      <Work
+        img="/lnkr.png"
+        url="https://lnkr.space"
+        title="Lnkr"
+        date="2020"
+        tag="React, TS, Chakra UI, Koa.js"
+        description="Link shortening with QR-code"
+      />
+      <Work
+        img="/todo.png"
+        url="https://todo.enotcode.com"
+        title="ToDo"
+        date="2020"
+        tag="Vue, Vuetify"
+        description="Simple ToDo list with badges"
       />
     </div>
   );
